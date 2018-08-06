31954b03ee1fadbd4b05866ef03a48364d23e4db
JJ0424 <jj792714612@outlook.com>
2018-08-06 09:53:52 +0800

    2018-08-06 1. fifo改版

--- middle layer/fifo/single FIFO/fifo.c
+++ middle layer/fifo/single FIFO/fifo.c
@@ -1,199 +1,158 @@
 
-// v0.1 2017-6-6
-// 1. Add FiFoReadTxBytes()
+/*----------------------------------------------------------------------^^-
+/ File name:  fifo.c
+/ Author:     JiangJun
+/ Data:       2018/8/2
+/ Version:    v0.1
+/-----------------------------------------------------------------------^^-
+/ FIFO Driver
+/------------------------------------------------------------------------*/
 
 #include "fifo.h"
 
-//------------------------------------------------------------
-// FIFO Buffer
-//------------------------------------------------------------
-FiFoChannelT FiFoChannel;
-
-
-// Clear the all data of the buffer.
-void FiFoReset(void)
+/*----------------------------------------------------------------------
+ *  FiFoSpxInit
+ *
+ *  Purpose: None.
+ *  Entry:   None.
+ *  Exit:    None.
+ *  NOTE:    None.
+ *---------------------------------------------------------------------*/
+void FiFoSpxInit(FiFoSpxT *dpx, _u8_t *buff, _idx_u16_t buff_len)
 {
-#if ENABLE_FIFO_TX
 
-    FiFoChannel.transmit.write_index = 0;
-    FiFoChannel.transmit.read_index = 0;
-    FiFoChannel.transmit.count = 0;
-    
-#endif    
+    // Init
+    dpx->_w_idx = 0; dpx->_r_idx = 0;
+    dpx->_cnt = 0; dpx->_total_cnt = 0; dpx->_full = 0;
+    if (buff != 0) {
 
-#if ENABLE_FIFO_RX
-
-    FiFoChannel.receive.write_index = 0;
-    FiFoChannel.receive.read_index = 0;
-    FiFoChannel.receive.count = 0;
-
-#endif
-}
-
-#if ENABLE_FIFO_TX
-
-// Write Tx FIFO
-void FiFoWriteTx(u8 *dat, IndexT cnt)
-{
-    IndexT index = 0;
-    
-    for (; cnt != 0; dat++, cnt--)
-    {
-    	index = FiFoChannel.transmit.count;
-    	
-    	if (index < sizeof(FiFoChannel.transmit.buffer))
-    	{
-    		(FiFoChannel.transmit.count)++;
-    		index = FiFoChannel.transmit.write_index;
-    		FiFoChannel.transmit.buffer[index] = *dat;
-    		FiFoChannel.transmit.write_index = (index + 1) % sizeof(FiFoChannel.transmit.buffer);	
-    	}
-    	else
-    	{                
-            return;
-    	}
+        dpx->buff = buff; dpx->buff_len = buff_len;
     }
+    else {
+
+        dpx->buff = 0; dpx->buff_len = 0;
+    }    
 }
 
-// return the number of the Tx FIFO
-IndexT FiFoReadTx(u8 *dat)
+/*----------------------------------------------------------------------
+ *  FiFoSpxWrite - Write FIFO
+ *
+ *  Purpose: None.
+ *  Entry:   None.
+ *  Exit:    None.
+ *  NOTE:    None.
+ *---------------------------------------------------------------------*/
+void FiFoSpxWrite(FiFoSpxT *spx, _u8_t *dat, _idx_u16_t cnt)
 {
-    IndexT index = 0, cnt = 0;
-	
-	for (cnt = 0; cnt < FIFO_SIZE; cnt++)
-	{
-	    index = FiFoChannel.transmit.count;
-		
-    	// Not Empty
-    	if (index)
-    	{
-    		FiFoChannel.transmit.count = (--index);	
-    		index = FiFoChannel.transmit.read_index;
-    		dat[cnt] = FiFoChannel.transmit.buffer[index];    		
-    		FiFoChannel.transmit.read_index = (index + 1) % sizeof(FiFoChannel.transmit.buffer);
-    	}
-    	else
-    	{
-    	    break;
-    	}
-	}
-	
-	return cnt;
-}
 
-// return bytes of the Tx FIFO with specified count.
-IndexT FiFoReadTxBytes(u8 *dat, IndexT num)
-{
-    IndexT index = 0, cnt = 0;
-	
-	for (cnt = 0; (cnt < FIFO_SIZE) && (num != 0); cnt++, num--)
-	{
-	    index = FiFoChannel.transmit.count;
-		
-    	// Not Empty
-    	if (index)
-    	{
-    		FiFoChannel.transmit.count = (--index);	
-    		index = FiFoChannel.transmit.read_index;
-    		dat[cnt] = FiFoChannel.transmit.buffer[index];    		
-    		FiFoChannel.transmit.read_index = (index + 1) % sizeof(FiFoChannel.transmit.buffer);
-    	}
-    	else
-    	{
-    	    break;
-    	}
-	}
-	
-	return cnt;
-}
+    _idx_u16_t idx = 0; _u8_t *dat_tmp = dat; FiFoSpxT *spx_tmp = spx;
 
-IndexT FiFoGetTxCount(void)
-{
-    return FiFoChannel.transmit.count;
-}
+    // total count +
+    spx_tmp->_total_cnt += cnt;
 
-#endif /* ENABLE_FIFO_TX */
+    // check
+    if ((spx_tmp->buff == 0) || (spx_tmp->buff_len == 0)) {
 
-#if ENABLE_FIFO_RX
-
-// Write Rx FIFO
-void FiFoWriteRx(u8 *dat, IndexT cnt)
-{
-    IndexT index = 0;
-    
-    for (; cnt != 0; dat++, cnt--)
-    {
-    	index = FiFoChannel.receive.count;
-    	
-    	if (index < sizeof(FiFoChannel.receive.buffer))
-    	{
-    		(FiFoChannel.receive.count)++;
-    		index = FiFoChannel.receive.write_index;
-    		FiFoChannel.receive.buffer[index] = *dat;
-    		FiFoChannel.receive.write_index = (index + 1) % sizeof(FiFoChannel.receive.buffer);	
-    	}
-    	else
-    	{                
-            return;
-    	}
+        spx_tmp->_full = 0xFF;
+        return;
     }
+           
+    // b2b write
+    for (idx = 0; idx < cnt; idx++)
+    {
+
+        spx_tmp->buff[spx_tmp->_w_idx] = *dat_tmp; dat_tmp++;
+        spx_tmp->_w_idx++; spx_tmp->_w_idx %= spx_tmp->buff_len;
+        
+    	// not full
+    	if (spx_tmp->_cnt < spx_tmp->buff_len) { spx_tmp->_cnt++; }
+    }
+
+    // full check
+    if (spx_tmp->_cnt >= spx_tmp->buff_len) { spx_tmp->_full = 0xFF; }
 }
 
-// return the number of the Rx FIFO
-IndexT FiFoReadRx(u8 *dat)
+/*----------------------------------------------------------------------
+ *  FiFoSpxReadBytes
+ *
+ *  Purpose: None.
+ *  Entry:   None.
+ *  Exit:    None.
+ *  NOTE:    None.
+ *---------------------------------------------------------------------*/
+_idx_u16_t FiFoSpxReadBytes(FiFoSpxT *spx, _u8_t *dat, _idx_u16_t cnt)
 {
-    IndexT index = 0, cnt = 0;
-	
-	for (cnt = 0; cnt < FIFO_SIZE; cnt++)
+
+    _idx_u16_t idx = 0; _u8_t *dat_tmp = dat; FiFoSpxT *spx_tmp = spx;
+
+	// check
+    if ((spx_tmp->buff == 0) || (spx_tmp->buff_len == 0)) {
+        
+        return 0;
+    }
+
+    // b2b read
+    for (idx = 0; (idx < cnt) && (idx < spx_tmp->buff_len); idx++)
 	{
-	    index = FiFoChannel.receive.count;
-		
-    	// Not Empty
-    	if (index)
-    	{
-    		FiFoChannel.receive.count = (--index);	
-    		index = FiFoChannel.receive.read_index;
-    		dat[cnt] = FiFoChannel.receive.buffer[index];    		
-    		FiFoChannel.receive.read_index = (index + 1) % sizeof(FiFoChannel.receive.buffer);
-    	}
-    	else
-    	{
-    	    break;
-    	}
+
+        // not empty
+    	if (spx_tmp->_cnt == 0) { break; }
+    	
+	    *dat_tmp = spx_tmp->buff[spx_tmp->_r_idx]; dat_tmp++;
+	    spx_tmp->_r_idx++; spx_tmp->_r_idx %= spx_tmp->buff_len;
+	    spx_tmp->_cnt--;
 	}
+
+	// full clear
+	if (spx_tmp->_cnt < spx_tmp->buff_len) { spx_tmp->_full = 0x00; }
 	
-	return cnt;
+	return idx;
 }
 
-// return bytes of the Rx FIFO with specified count.
-IndexT FiFoReadRxBytes(u8 *dat, IndexT num)
+/*----------------------------------------------------------------------
+ *  FiFoSpxGetCount
+ *
+ *  Purpose: None.
+ *  Entry:   None.
+ *  Exit:    None.
+ *  NOTE:    None.
+ *---------------------------------------------------------------------*/
+_idx_u16_t FiFoSpxGetCount(FiFoSpxT *spx)
 {
-    IndexT index = 0, cnt = 0;
-	
-	for (cnt = 0; (cnt < FIFO_SIZE) && (num != 0); cnt++, num--)
-	{
-	    index = FiFoChannel.receive.count;
-		
-    	// Not Empty
-    	if (index)
-    	{
-    		FiFoChannel.receive.count = (--index);	
-    		index = FiFoChannel.receive.read_index;
-    		dat[cnt] = FiFoChannel.receive.buffer[index];    		
-    		FiFoChannel.receive.read_index = (index + 1) % sizeof(FiFoChannel.receive.buffer);
-    	}
-    	else
-    	{
-    	    break;
-    	}
-	}
-	
-	return cnt;
+
+    return spx->_cnt;
 }
 
-IndexT FiFoGetRxCount(void)
+/*----------------------------------------------------------------------
+ *  FiFoSpxIsEmpty - 0xFF Empty
+ *
+ *  Purpose: None.
+ *  Entry:   None.
+ *  Exit:    None.
+ *  NOTE:    None.
+ *---------------------------------------------------------------------*/
+_u8_t FiFoSpxIsEmpty(FiFoSpxT *spx)
 {
-    return FiFoChannel.receive.count;
+
+    if (spx->_cnt == 0) { return 0xFF; }
+    return 0x00;
 }
 
-#endif /* ENABLE_FIFO_RX */
+/*----------------------------------------------------------------------
+ *  FiFoSpxIsFull - 0xFF Full
+ *
+ *  Purpose: None.
+ *  Entry:   None.
+ *  Exit:    None.
+ *  NOTE:    None.
+ *---------------------------------------------------------------------*/
+_u8_t FiFoSpxIsFull(FiFoSpxT *spx)
+{
+
+    if (spx->_full) { return 0xFF; }
+    return 0x00;
+}
+
+//---------------------------------------------------------------------------//
+//----------------------------- END OF FILE ---------------------------------//
+//---------------------------------------------------------------------------//
--- middle layer/fifo/single FIFO/fifo.h
+++ middle layer/fifo/single FIFO/fifo.h
@@ -1,55 +1,56 @@
 
+/*----------------------------------------------------------------------^^-
+/ File name:  fifo.c
+/ Author:     JiangJun
+/ Data:       2018/8/2
+/ Version:    v0.1
+/-----------------------------------------------------------------------^^-
+/ FIFO Driver
+/------------------------------------------------------------------------*/
+
 #ifndef _FIFO_H
 #define _FIFO_H
 
-#include "main.h"
+//------------------------------------------------------------
+//              TYPEDEF
+//------------------------------------------------------------
 
-#define ENABLE_FIFO_TX  1
-#define ENABLE_FIFO_RX  1
-
-#define FIFO_SIZE	        512     // UNIT: Byte
+typedef unsigned char _u8_t;
+typedef unsigned short _idx_u16_t;
+typedef unsigned int _idx_u32_t;
 
 //------------------------------------------------------------
-// FIFO Data Struct Type define.
+//              FIFO Simplex Channel
 //------------------------------------------------------------
-#if FIFO_SIZE >= 256
-	typedef u16	IndexT;
-#else
-	typedef u8  IndexT;
-#endif
 
 typedef struct {
-	IndexT write_index;
-	IndexT read_index;
-	IndexT count;
-	u8 buffer[FIFO_SIZE];
-} FIFOT;
+
+	_idx_u16_t _w_idx;
+	_idx_u16_t _r_idx;
+	_idx_u16_t _cnt;
+	_idx_u32_t _total_cnt;
+	_u8_t *buff;
+	_idx_u16_t buff_len;
+	_u8_t _full;
+	
+} FiFoSpxT;
 
 //------------------------------------------------------------
-// The structure of the one UART.
+//              FIFO Duplex Channel
 //------------------------------------------------------------
+
 typedef struct {
 
-#if ENABLE_FIFO_TX
-    FIFOT transmit;
-#endif
+    FiFoSpxT _tx_channel;
+    FiFoSpxT _rx_channel;
+    
+} FiFoDpxT;
 
-#if ENABLE_FIFO_RX
-    FIFOT receive;
-#endif
-} FiFoChannelT;
-
-void FiFoReset(void);
-
-void FiFoWriteTx(u8 *dat, IndexT cnt);
-IndexT FiFoReadTx(u8 *dat);
-IndexT FiFoReadTxBytes(u8 *dat, IndexT num);
-IndexT FiFoGetTxCount(void);
-
-void FiFoWriteRx(u8 *dat, IndexT cnt);
-IndexT FiFoReadRx(u8 *dat);
-IndexT FiFoReadRxBytes(u8 *dat, IndexT num);
-IndexT FiFoGetRxCount(void);
-
+extern void FiFoSpxInit(FiFoSpxT *dpx, _u8_t *buff, _idx_u16_t buff_len);
+extern void FiFoSpxWrite(FiFoSpxT *spx, _u8_t *dat, _idx_u16_t cnt);
+extern _idx_u16_t FiFoSpxReadBytes(FiFoSpxT *spx, _u8_t *dat, _idx_u16_t cnt);
+extern _idx_u16_t FiFoSpxGetCount(FiFoSpxT *spx);
+extern _u8_t FiFoSpxIsEmpty(FiFoSpxT *spx);
+extern _u8_t FiFoSpxIsFull(FiFoSpxT *spx);
 
 #endif
